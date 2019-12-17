aaff01f7318dd568176839daef07d2a47f7c607f
Artie <turo310@gmail.com>
2019-12-17 15:13:03 -0800

    Add information to skills section, update cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -156,9 +156,8 @@ const IndexPage = (props) => (
           <div className="card">
             <div className="face face--2">
               <div className="content">
-                <p>djbshjcbdshjcsd cjbndskjv s dvhs divhj
-                jsndkjcbsbdncbsdh cjhds cb dhs ck sdkc
-                shjd ckjh nsdchj nhjus dcjdscj d cdkjs</p>
+                <p>Detail your car by the best in the business.</p>
+                <p>Made With: GatsbyJS, HTML5, CSS3</p>
                 <a href="#">Github</a>
                 <a href="#">Live Demo</a>
               </div>
@@ -174,16 +173,15 @@ const IndexPage = (props) => (
           <div className="card">
             <div className="face face--2">
               <div className="content">
-                <p>djbshjcbdshjcsd cjbndskjv s dvhs divhj
-                jsndkjcbsbdncbsdh cjhds cb dhs ck sdkc
-                shjd ckjh nsdchj nhjus dcjdscj d cdkjs</p>
+                <p>Search Mars rover pictures by date through NASA's API.</p>
+                <p>Made With: React, HTML5, CSS3, Javascript, NASA API, Context API, React Hooks</p>
                 <a href="#">Github</a>
                 <a href="#">Live Demo</a>
               </div>
             </div>
             <div className="face face--1">
               <div className="content">
-                <Img  alt="Gatsby"
+                <Img  alt="Nasa"
                       fluid={props.data.discover_mars.childImageSharp.fluid} />
               </div>
             </div>
@@ -192,16 +190,15 @@ const IndexPage = (props) => (
           <div className="card">
             <div className="face face--2">
               <div className="content">
-                <p>djbshjcbdshjcsd cjbndskjv s dvhs divhj
-                jsndkjcbsbdncbsdh cjhds cb dhs ck sdkc
-                shjd ckjh nsdchj nhjus dcjdscj d cdkjs</p>
+                <p>Top notch insulated outerwear ecommerce business.</p>
+                <p>Made With: NextJS, Node, ContextAPI, React Hooks</p>
                 <a href="#">Github</a>
                 <a href="#">Live Demo</a>
               </div>
             </div>
             <div className="face face--1">
               <div className="content">
-                <Img  alt="Gatsby"
+                <Img  alt="Nexfit"
                       fluid={props.data.nexfit.childImageSharp.fluid} />
               </div>
             </div>
@@ -214,31 +211,79 @@ const IndexPage = (props) => (
     <section className="section my-skills">
       <div className="my-skills__container">
         <h1>My Skills</h1>
+
         <div className="my-skills__item-container">
           <div className="my-skills__card">
-            <h2>Skill 1</h2>
-            <p>Skills stuff here</p>
+            <div className="my-skills__card-title">
+              <h2>The Basics</h2>
+            </div>
+            <p><span className="skill-title">HTML5</span></p>
+            <p><span className="skill-title">CSS3: </span>Flexbox, Grid, Animations, BEM</p>
+            <p><span className="skill-title">Preprocessors: </span>Sass</p>
+            <p><span className="skill-title">CSS Frameworks: </span>Bootstrap, MaterialUI, etc.</p>
           </div>
+
           <div className="my-skills__card">
-            <h2>Skill 1</h2>
-            <p>Skills stuff here</p>
+            <div className="my-skills__card-title">
+              <h2>Javascript</h2>
+            </div>
+            <p>ES5, ES6, ES7, ES8</p>
+            <p><span className="skill-title">Fundamentals: </span>Control Flow, Error Handling, Data Models, Asynchronity, DOM Manipulation, Functional Programming, OOP, MVC/MVVM etc...</p>
           </div>
+
           <div className="my-skills__card">
-            <h2>Skill 1</h2>
-            <p>Skills stuff here</p>
+            <div className="my-skills__card-title">
+              <h2>Front End Frameworks</h2>
+            </div>
+            <p><span className="skill-title">ReactJS: </span>A Javascript library for building user interfaces</p>
+            <p><span className="skill-title">GatsbyJS: </span>A blazing fast static site generator for React, used for Client Side Rendering</p>
+            <p><span className="skill-title">NextJS: </span>Write server-rendered React apps easily</p>
           </div>
+
           <div className="my-skills__card">
-            <h2>Skill 1</h2>
-            <p>Skills stuff here</p>
+            <div className="my-skills__card-title">
+              <h2>State Management</h2>
+            </div>
+            <p>Context API, React Hooks</p>
+            <p>React Redux</p>
+            <p>Props, localStorage</p>
           </div>
+
           <div className="my-skills__card">
-            <h2>Skill 1</h2>
-            <p>Skills stuff here</p>
+            <div className="my-skills__card-title">
+              <h2>Build Tools</h2>
+            </div>
+            <p>Node Package Manager (NPM)</p>
+            <p>Yarn</p>
+            <p>Webpack</p>
+            <p>Babel</p>
           </div>
+
           <div className="my-skills__card">
-            <h2>Skill 1</h2>
-            <p>Skills stuff here</p>
+            <div className="my-skills__card-title">
+              <h2>Back End</h2>
+            </div>
+            <p>Node.js, Express, Web APIs</p>
+            <p>MySQL, MongoDB, Firebase</p>
           </div>
+
+          <div className="my-skills__card">
+            <div className="my-skills__card-title">
+              <h2>Optimization</h2>
+            </div>
+            <p>Image Optimization</p>
+            <p>Cross-Browser/Platform Testing, Accessibility, Mobile Web Performance</p>
+          </div>
+
+          <div className="my-skills__card">
+            <div className="my-skills__card-title">
+              <h2>Additional</h2>
+            </div>
+            <p>If you are interested and have time, please feel free to check out my github 
+            to review my code which may include skills not on here, as well as previous Android Development Projects</p>
+
+          </div>
+
         </div>
       </div>
     </section>
